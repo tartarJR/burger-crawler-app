73695b178b839986223d42e7df88913f5e5527b9
tartarJR <mustafaogun.ozturk@gmail.com>
2019-02-03 23:05:26 +0300

    defined bak end api end point

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  ROOT_API: '"https://burger-crawler.herokuapp.com/api/"',
+  BURGER_VENUES_END_POINT: '"venues/burger-venues"'
 })
