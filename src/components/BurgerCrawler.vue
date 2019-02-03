91f9c1d5405ec6a541fa2ca81cde32fb09cc7db4
tartarJR <mustafaogun.ozturk@gmail.com>
2019-02-03 23:55:15 +0300

    displaying venues now

--- src/components/BurgerCrawler.vue
+++ src/components/BurgerCrawler.vue
@@ -1,5 +1,32 @@
 <template>
+
+  <div>
   
+    <nav class="navbar navbar-dark bg-dark">
+      <span class="navbar-brand mb-0 h1">Burger Crawler</span>
+    </nav>
+
+    <div class="container">
+
+      <div class="row">
+
+        <div v-for="(venue, i) in venues" :key="i" class="col mt-4 mb-4">
+
+          <div class="card" style="width: 18rem;">
+            <div class="card-header">
+              <h5>{{ venue.venueName }}</h5>
+            </div>
+            <img class="card-img-top" :src="venue.latestBurgerPhotoUrl">
+          </div>
+    
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
 </template>
 
 <script>
@@ -21,6 +48,7 @@
         this.getVenues()
 
     },
+
     methods: {
     
         getVenues: function() {
