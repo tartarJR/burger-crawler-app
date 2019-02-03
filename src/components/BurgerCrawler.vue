806acf5de04e848cad4d87ca89dabe1abf0bb39d
tartarJR <mustafaogun.ozturk@gmail.com>
2019-02-03 23:09:49 +0300

    get request for retrieving burger venues addded

--- src/components/BurgerCrawler.vue
+++ src/components/BurgerCrawler.vue
@@ -4,9 +4,41 @@
 
 <script>
 
+  import axios from "axios";
+
   export default {
 
+    data: function(){
+        return {
+      
+            venues: []
+
+        }
+    },
+
+    mounted() {
     
+        this.getVenues()
+
+    },
+    methods: {
+    
+        getVenues: function() {
+        
+            axios.get(process.env.ROOT_API + process.env.BURGER_VENUES_END_POINT)
+                    .then(response => {
+
+                      console.log(response.data);
+
+                      this.venues = response.data
+                    
+                    }).catch(error => {
+                        
+                        console.log(error);
+                    });
+        }
+    
+    }
 
   };
 
