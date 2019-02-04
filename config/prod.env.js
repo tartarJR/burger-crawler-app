b185f092dd7327d75faa143299330ed5856d7f32
tartarJR <mustafaogun.ozturk@gmail.com>
2019-02-04 07:27:55 +0300

    added and configured node server

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  ROOT_API: '"https://burger-crawler.herokuapp.com/api/"',
+  BURGER_VENUES_END_POINT: '"venues/burger-venues"'
 }
